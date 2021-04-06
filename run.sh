d1380505f484394ed35538b28cfd50a56c89b31c
G <52905881+giga-a@users.noreply.github.com>
2021-04-06 12:35:26 -0700

    Add files via upload

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 echo -e "\nQeeqBox Seahorse v$(jq -r '.version' info) starter script -> https://github.com/qeeqbox/seahorse"
 echo -e "Current servers (HTTP, HTTPS, SSH, FTP, RDP, VNC, SMB, MONGO, MYSQL and LDAP)\n"
 
+
 setup_requirements () {
 	sudo apt update -y
 	sudo apt install -y linux-headers-$(uname -r) docker.io jq xdg-utils curl
@@ -11,24 +12,38 @@ setup_requirements () {
 }
 
 wait_on_web_interface () {
-until $(curl --silent --head --fail http://localhost:5601/login --output /dev/null); do
+until $(curl --silent --head --fail http://localhost:3000 --output /dev/null); do
 sleep 5
 done
-xdg-open http://localhost:5601/login
+xdg-open http://localhost:3000
 }
 
-dev_project () {
+test_project () {
 	docker-compose -f docker-compose-test.yml up --build
 }
 
+dev_project () {
+	docker-compose -f docker-compose-dev.yml up --build
+}
+
 stop_containers () {
-	docker-compose -f docker-compose-test.yml down -v
-	docker stop $(docker ps | grep seahorse_ | awk '{print $1}') 2>/dev/null
-	docker kill $(docker ps | grep seahorse_ | awk '{print $1}') 2>/dev/null
+	docker-compose -f docker-compose-test.yml down -v 2>/dev/null
+	docker-compose -f docker-compose-dev.yml down -v 2>/dev/null
+	docker stop $(docker ps | grep chameleon_ | awk '{print $1}') 2>/dev/null
+	docker kill $(docker ps | grep chameleon_ | awk '{print $1}') 2>/dev/null
 } 
 
 deploy_aws_project () {
 	echo "Will be added later on"
+}
+
+auto_configure_test () {
+	stop_containers
+	wait_on_web_interface & 
+	setup_requirements
+	test_project
+	stop_containers 
+	kill %% 2>/dev/null
 }
 
 auto_configure () {
@@ -40,16 +55,23 @@ auto_configure () {
 	kill %% 2>/dev/null
 }
 
+if [[ "$1" == "auto_configure_test" ]]; then
+	auto_configure_test
+fi
+
 if [[ "$1" == "auto_configure" ]]; then
 	auto_configure
 fi
 
 kill %% 2>/dev/null
 
-while read -p "`echo -e '\nChoose an option:\n1) Setup requirements (docker, docker-compose)\n2) Run auto configuration test\n>> '`"; do
+while read -p "`echo -e '\nChoose an option:\n1) Setup requirements (docker, docker-compose)\n2) Test the project (All servers and Sniffer)\n8) Run auto configuration\n9) Run auto test\n>> '`"; do
   case $REPLY in
     "1") setup_requirements;;
-    "2") auto_configure;;
+    "2") test_project;;
+    "8") auto_configure;;
+    "9") auto_configure_test;;
     *) echo "Invalid option";;
   esac
 done
+
